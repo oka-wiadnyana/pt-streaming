5f843af18ce5f2ccf5a9e759cb71cc78b3e26986
Oka Wiadnyana <onsdee86@gmail.com>
2024-07-15 13:16:40 +0800

    fix: logo in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title>Agroxa - Responsive Bootstrap 4 Admin Dashboard</title>
+    <title>Si Putri</title>
     <meta content="Admin Dashboard" name="description" />
     <meta content="Themesbrand" name="author" />
     <link rel="shortcut icon" href="{{ asset('template_assets') }}/images/favicon.ico">
